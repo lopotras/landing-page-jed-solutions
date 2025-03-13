cd43373534797299e472eaf73847d0599fecbe6c
lopotras <63056046+lopotras@users.noreply.github.com>
2025-03-13 17:29:45 +0100

    fix: Remove base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: '/landing-page/',
   plugins: [react()],
   resolve: {
     alias: {
